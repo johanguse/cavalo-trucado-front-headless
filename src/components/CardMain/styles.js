c6f5b1f67c5eabb26763113c492d194aa7e04007
Johan Guse <johanguse@gmail.com>
2022-02-17 17:20:35 -0300

    config tailwind

---
+++ src/components/CardMain/styles.js
@@ -0,0 +1,14 @@
+import tw from "twin.macro";
+
+export const BgCard = tw.div`
+  bg-red-500
+  hover:bg-red-700
+  text-white
+  font-bold
+  py-2
+  px-4
+  border
+  border-black
+  rounded
+  mb-10
+`;
