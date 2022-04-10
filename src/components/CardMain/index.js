1e2b9611476df53ff42a9237024e2c13de4fa55c
Johan Guse <johanguse@gmail.com>
2022-04-10 16:29:12 -0300

    improve maincard layout

--- src/components/CardMain/index.js
+++ src/components/CardMain/index.js
@@ -4,11 +4,6 @@ import { BgCard, ImgCard, TextCard } from "./styles";
 import Truck from "./truck";
 
 export default function CardMain({ vehicles }) {
-
-  console.log(vehicles)
-  //let brandName = vehicles.brands.nodes[0].name;
-  //console.log(brandName)
-
   return (
     <main className="flex items-center justify-center flex-1 w-full px-2 py-8 bg-gray-100">
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-6 max-w-7xl sm:px-4 lg:px-8">
--- src/components/CardMain/truck.js
+++ src/components/CardMain/truck.js
@@ -3,39 +3,61 @@ import Image from "next/image";
 import { BgCard, ImgCard, TextCard } from "./styles";
 
 const Truck = ({ vehicle }) => {
-  /*
-    if (isEmpty(product)) {
-      return null;
-    }
-  
-    const img = product?.images?.[0] ?? {};
-    const productType = product?.type ?? '';
-  */
   const brandName = vehicle.brands.nodes[0].name;
+  const showPrice = vehicle.vehicle_infos.vehicleShowPrice;
+  const state = vehicle.vehicle_infos.vehicleState;
+  const year = vehicle.vehicle_infos.vehicleYear;
+  const yearModel = vehicle.vehicle_infos.vehicleYearModel;
   return (
     <BgCard key={vehicle.vehicleId}>
-      <ImgCard>
-        <Image
-          className="rounded-t-lg"
-          src={vehicle.vehicle_infos.vehicleMainPhoto.sourceUrl}
-          alt={vehicle.vehicle_infos.vehicleModelName}
-          width="480"
-          height="560"
-          objectFit="cover"
-        />
-      </ImgCard>
+      <Link href={`/caminhao/${encodeURIComponent(vehicle.slug)}`}>
+        <ImgCard>
+          <Image
+            className="rounded-t-lg"
+            src={vehicle.vehicle_infos.vehicleMainPhoto.sourceUrl}
+            alt={vehicle.vehicle_infos.vehicleModelName}
+            width="480"
+            height="560"
+            objectFit="cover"
+          />
+        </ImgCard>
+      </Link>
       <TextCard>
-        <h2 className="text-2xl">
-          <span>{brandName}</span>
-          <span className="ml-2 text-red-700">{vehicle.vehicle_infos.vehicleModelName}</span>
-        </h2>
-        <p>{vehicle.vehicleId}</p>
-        <p>{vehicle.vehiclePrice}</p>
-        <br />
-        <Link href={`/caminhao/${encodeURIComponent(vehicle.slug)}`}>Link</Link>
-        <br /><br />
+        <Link href={`/caminhao/${encodeURIComponent(vehicle.slug)}`}>
+          <h2 className="mb-2 text-2xl cursor-pointer">
+            <span>{brandName}</span>
+            <span className="ml-2 text-red-700">{vehicle.vehicle_infos.vehicleModelName}</span>
+          </h2>
+        </Link>
+        <div className="price mb-1.5">
+          {(showPrice != true || showPrice == null) ?
+            <p>{vehicle.vehicle_infos.vehiclePrice?.toLocaleString('pt-BR', { minimumFractionDigits: 3, style: 'currency', currency: 'BRL' }) || ''}</p>
+            :
+            <p>Sob Consulta</p>
+          }
+        </div>
+        <div className="flex flex-row justify-around mb-1.5 text-sm text-gray-500 short-descritpions">
+          <span>{vehicle.vehicle_infos.vehicleShortText1}</span>
+          <span>{vehicle.vehicle_infos.vehicleShortText2}</span>
+          <span>{vehicle.vehicle_infos.vehicleShortText3}</span>
+        </div>
+        <div className="flex flex-row mb-1.5 text-xs text-gray-700 year-km">
+          <div className="year">Ano: {year} {yearModel && " / " + yearModel}</div>
+          <div className="km">km: {vehicle.vehicle_infos.vehicleKm?.toFixed(3) || ''}</div>
+        </div>
+        <div className="flex flex-row text-xs text-gray-500 state">
+          <div className="pr-1 icon">
+            a
+          </div>
+          {(state == null || state == "NO") ?
+            <p>Brasil</p>
+            :
+            <p>{vehicle.vehicle_infos.vehicleState} </p>
+          }
+        </div>
       </TextCard>
-    </BgCard>
+
+    </BgCard >
   )
 }
 
