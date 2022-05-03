0fbc35624ad64e456fd77555db1bba5071d4d3f8
Johan Guse <johanguse@gmail.com>
2022-05-03 14:53:12 -0300

    improve contact page and conditionals

--- src/components/CardMain/index.js
+++ src/components/CardMain/index.js
@@ -6,11 +6,11 @@ export default function CardMain({ vehicles }) {
   return (
     <main className="flex items-center justify-center flex-1 w-full px-2 py-8 bg-gray-50">
       <div className="container grid grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-6 max-w-7xl sm:px-4 lg:px-8">
-        {vehicles.length ? vehicles.map(vehicles => {
+        {vehicles.length && vehicles.map(vehicles => {
           return (
             <Truck key={vehicles?.vehicleId} vehicle={vehicles} />
           )
-        }) : null}
+        })}
       </div>
     </main>
   )
